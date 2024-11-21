9ea720ed47b298533b872f0827083fbf802aa0d9
purwar1997 <shubhampurwar511@gmail.com>
2024-11-21 18:02:05 +0530

    Define all the permissions

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,17 @@
 export const STORAGE = Object.freeze({
   DATABASE_NAME: 'rbac_db',
 });
+
+export const PERMISSIONS = Object.freeze({
+  VIEW_USERS: 'view users',
+  ADD_USER: 'add user',
+  EDIT_USER: 'edit user',
+  DELETE_USER: 'delete user',
+  ACTIVATE_USER: 'activate user',
+  DEACTIVATE_USER: 'deactivate user',
+  ARCHIVE_USER: 'archive user',
+  RESTORE_USER: 'restore user',
+  VIEW_ROLES: 'view roles',
+  ADD_ROLE: 'add role',
+  EDIT_ROLE: 'edit role',
+});
